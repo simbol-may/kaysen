3452f064f143c17566294122e5d1c0e8da7b1507
吴冰媚 <8753742+wu-bingmei@user.noreply.gitee.com>
2021-09-25 17:49:01 +0800

    修改样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -168,7 +168,7 @@ $cursor: #000;
       caret-color: $cursor;
 
       &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
+        // box-shadow: 0 0 0px 1000px $bg inset !important;
         -webkit-text-fill-color: $cursor !important;
       }
     }
